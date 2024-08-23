66e1aa0e3faa7d0e815206a76bb52be711798d00
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-23 08:54:40 +0000

    ask next question

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from data import question_data
 from question import Question
+from quiz_brain import QuizBrain
 def main() -> None:
-    
+
     question_bank = []
     for question in question_data:
         question_bank.append(Question(question["text"], question["answer"]))
-    
-    print(question_bank)
+
+    quiz = QuizBrain(question_bank)
+    quiz.next_question()
 
 if __name__ =="__main__":
     main()
