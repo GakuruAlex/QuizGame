4c5ad6be803f44576552b9b0d71b22ef2892fb28
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-23 09:16:23 +0000

    show current score

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,17 +7,19 @@ class QuizBrain:
     def still_has_questions(self):
         return self.question_number < len(self.questions_list)
     def check_answer(self, user_answer, correct_answer):
-        if user_answer == correct_answer:
+        if user_answer == correct_answer.lower():
             print("Thats Right!")
             self.score += 1
         else:
             print("You got it wrong!")
         print(f"The correct answer is: {correct_answer}")
+        print(f"Your current score is {self.score}/{self.question_number}")
 
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
 
         answer = input(f"Q.{self.question_number} {current_question.text}. (True/False)?: ").lower()
+        self.check_answer(answer, current_question.answer)
 
 
