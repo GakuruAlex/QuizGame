7759e16b30486a17da198bdf91179e7a16b91e43
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-23 09:20:12 +0000

    doc string

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,18 +4,32 @@ class QuizBrain:
         self.questions_list = q_bank
         self.score = 0
 
-    def still_has_questions(self):
+    def still_has_questions(self) -> bool:
+        """_Check if there are more questions_
+
+        Returns:
+            bool: _True if more questions are in the question list otherwise False_
+        """
         return self.question_number < len(self.questions_list)
-    def check_answer(self, user_answer, correct_answer):
+
+    def check_answer(self, user_answer: str, correct_answer: str) -> None:
+        """_Check if user answer is correct_
+
+        Args:
+            user_answer (str): _Answer given by user_
+            correct_answer (str): _The correct answer to the question_
+        """
         if user_answer == correct_answer.lower():
             print("Thats Right!")
             self.score += 1
         else:
             print("You got it wrong!")
         print(f"The correct answer is: {correct_answer}")
-        print(f"Your current score is {self.score}/{self.question_number}")
+        print(f"Your score {self.score}/{self.question_number}")
 
-    def next_question(self):
+    def next_question(self) -> None:
+        """_Get the next question_
+        """
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
 
