aac1e6f8c064f8171968612073ffc8cd8c73bd52
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-23 07:47:55 +0000

    Quiz game

---
+++ question.py
@@ -0,0 +1,13 @@
+class Question:
+    """_Model of Question_
+    """
+    def __init__(self, text: str, answer: bool):
+        """_Construct Question object_
+
+        Args:
+            text (_str_): _Question text_
+            answer (_boolean_): _Answer to the Question_
+        """
+        self.text = text
+        self.answer = answer
+        
